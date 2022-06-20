40da2aece636ab97e3753009f025feb6a3b980bd
MykolaHrytsenko <niksandler@gmail.com>
2022-06-21 01:46:51 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,40 +1,143 @@
+import './sass/main.scss';
 import { fetchImages } from './js/fetchImages';
 import { renderGallery } from './js/renderGallery';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const refs = {
-    searchForm: document.querySelector('#search-form'),
-    gallery: document.querySelector('.gallery')
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+// const loadMoreBtn = document.querySelector('.btn-load-more');
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+searchForm.addEventListener('submit', onSearchForm);
+// loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+
+function onSearchForm(evt) {
+    evt.preventDefault();
+    window.scrollTo({ top: 0 });
+    page = 1;
+    query = evt.currentTarget.searchQuery.value.trim();
+    gallery.innerHTML = '';
+    // loadMoreBtn.classList.add('is-hidden');
+
+    if (query === '') {
+        Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
+        return;
+    }
+
+    fetchImages(query, page, perPage)
+        .then(({ data }) => {
+            if (data.totalHits === 0) {
+                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            } else {
+                renderGallery(data.hits);
+                simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+                Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+                // if (data.totalHits > perPage) {
+                //     // loadMoreBtn.classList.remove('is-hidden');
+                //     smoothScroll();
+                // }
+            }
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+            searchForm.reset();
+        });
 }
 
-refs.searchForm.addEventListener('submit', onSubmit);
-
-function onSubmit(evt) {
-    evt.preventDefault();
-
-    const value = evt.target.value.trim();
-
-    // if (!value) {
-    //     return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = '');
-    // };
-
-    // fetchImages(value)
-    //     .then(name => {
-    //         refs.countryList.innerHTML = '';
-    //         refs.countryInfo.innerHTML = '';
-
-    //         if (name.length === 1) {
-    //             renderCountryInfo(name)
-    //             return
-    //         }
-
-    //         if (name.length >= 10) {
-    //             Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    //             return
-    //         }
-
-    //         else {
-    //             renderCountryList(name);
-    //         };
-    //     }).catch(onError);
-
+const infiniteScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (clientHeight + scrollTop >= scrollHeight) {
+        setTimeout(uploadMore, 1000);
+    }
 };
+// async function onScroll(evt) {
+//     if (
+//         document.documentElement.scrollHeight - document.documentElement.scrollTop <=
+//         document.documentElement.clientHeight + 1
+//     ) {
+//         fetchImages.currentPage += 1;
+//         try {
+//             const pics = await fetchImages;
+//             const picsOk = await isPicturesOver(pics);
+//             showPictures(picsOk);
+//             smoothScroll();
+//         } catch (error) {
+//             if (error?.response?.status === 400) {
+//                 Notiflix.Notify.failure('Page is out of valid range');
+//                 picturesApiService.currentPage -= 1;
+//             };
+
+//             if (error?.data?.hits?.length === 0) {
+//                 Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
+//                 picturesApiService.currentPage -= 1;
+//             };
+//         };
+//     };
+// };
+
+function smoothScroll() {
+    const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+    });
+};
+
+
+const handleScroll = () => {
+    infiniteScroll();
+};
+
+const uploadMore = () => {
+    fetchImages(searchForm.searchQuery.value, page)
+        .then(data => {
+            renderGallery(data);
+            scrollPage();
+            page += 1;
+        })
+        .catch(() => {
+            Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        });
+    console.log(fetchImages())
+};
+
+
+
+// async function isPicturesOver(response) {
+//     if (response.data.hits.length === 0) {
+//         throw response;
+//     }
+//     return response;
+// };
+
+// function onLoadMoreBtn() {
+//     page += 1;
+//     simpleLightBox.destroy();
+
+//     fetchImages(query, page, perPage)
+//         .then(({ data }) => {
+//             renderGallery(data.hits);
+//             simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+//             const totalPages = Math.ceil(data.totalHits / perPage);
+
+//             if (page > totalPages) {
+//                 loadMoreBtn.classList.add('is-hidden');
+//                 Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+//             }
+//         })
+//         .catch(error => console.log(error));
+// }
+
+window.addEventListener('scroll', handleScroll);
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,21 +1,12 @@
 import axios from 'axios';
+export { fetchImages };
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '28149926-6b971f2bd9468f5bcaa1af96d';
 
 async function fetchImages(query, page, perPage) {
-    const response = await axios.get(
-        `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
-    );
-    return response;
-
-    // return fetch(`${BASE_URL}/${countrieName}?fields=${URL_PARAM}`).then(
-    //     response => {
-    //         if (!response.ok || response.status === 404) {
-    //             return Promise.reject(new Error());
-    //         }
-    //         return response.json();
-    //     });
-};
-
-export { fetchImages };
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  );
+  return response;
+}
